62282bc9963c4cd09e0537c31c005f3fdc42191a
Elsie <hawiokatch@gmail.com>
2023-03-27 09:46:38 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,13 @@
+const divide = function() {
+    return 2000 / 100;
+  };
+  
+  var square = (num) => {
+    return num * num;
+  };
+  
+  var add = (num1, num2) => {
+    return num1 + num2;
+  };
+  
+  
